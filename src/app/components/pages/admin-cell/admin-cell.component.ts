939e7b5870da2ba1c91c85759e9f1e8e1162b935
Jerome Celle <jeffyer38130@gmail.com>
2018-05-09 10:51:23 +0200

    Add event creation

--- src/app/components/pages/admin-cell/admin-cell.component.ts
+++ src/app/components/pages/admin-cell/admin-cell.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CellService } from '../../../services/cell.service';
-import { Cell } from '../../../models/cell';
-import { Event } from '../../../models/event';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { EventService } from '../../../services/event.service';
-import { CycleService } from '../../../services/cycle.service';
-import { Cycle } from '../../../models/cycle';
-import { TasktypeService } from '../../../services/tasktype.service';
-import { Tasktype } from '../../../models/tasktype';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {CellService} from '../../../services/cell.service';
+import {Cell} from '../../../models/cell';
+import {Event} from '../../../models/event';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {EventService} from '../../../services/event.service';
+import {CycleService} from '../../../services/cycle.service';
+import {Cycle} from '../../../models/cycle';
+import {TasktypeService} from '../../../services/tasktype.service';
+import {Tasktype} from '../../../models/tasktype';
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {MyModalService} from '../../../services/my-modal/my-modal.service';
+import {NotificationsService} from 'angular2-notifications';
+import {AuthenticationService} from '../../../services/authentication.service';
 
 
 @Component({
@@ -31,12 +35,21 @@ export class AdminCellComponent implements OnInit {
   selectedCycles = [];
   dropdownCycleSettings = {};
 
+  eventForm: FormGroup;
+  cycles: Cycle[];
+  tasktypes: Tasktype[];
+
   constructor(private activatedRoute: ActivatedRoute,
               private eventService: EventService,
               private cellService: CellService,
               private cycleService: CycleService,
               private tasktypeService: TasktypeService,
-              private router: Router) {}
+              private router: Router,
+              private notificationService: NotificationsService,
+              private formBuilder: FormBuilder,
+              private authenticationService: AuthenticationService,
+              private myModals: MyModalService) {
+  }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params) => {
@@ -50,7 +63,7 @@ export class AdminCellComponent implements OnInit {
 
     this.cycleService.getCycles().subscribe(
       data => {
-        const cycles = data.results.map(c => new Cycle(c) );
+        const cycles = data.results.map(c => new Cycle(c));
         for (const cycle of Object.keys(cycles)) {
           this.dropdownCycleList.push(this.cycleToDict(cycles[cycle]));
         }
@@ -59,7 +72,7 @@ export class AdminCellComponent implements OnInit {
 
     this.tasktypeService.getTasktypes().subscribe(
       data => {
-        const tasktypes = data.results.map(t => new Tasktype(t) );
+        const tasktypes = data.results.map(t => new Tasktype(t));
         for (const tasktype of Object.keys(tasktypes)) {
           this.dropdownTasktypeList.push(this.tasktypeToDict(tasktypes[tasktype]));
         }
@@ -81,12 +94,14 @@ export class AdminCellComponent implements OnInit {
       unSelectAllText: 'Aucunes',
       classes: 'admin-cell__filters__filter',
     };
+
+    this.createForm();
   }
 
   get_events() {
     this.eventService.getEvents(this.cell.id).subscribe(
       data => {
-        this.events = data.results.map(e => new Event(e) );
+        this.events = data.results.map(e => new Event(e));
         this.filter();
       }
     );
@@ -141,10 +156,10 @@ export class AdminCellComponent implements OnInit {
 
     for (const event in this.events) {
       // If no task_type filter or filter is verified
-      if ( this.selectedTasktypes.length === 0
+      if (this.selectedTasktypes.length === 0
         || this.elemIsFiltered(this.tasktypeToDict(this.events[event].task_type), this.selectedTasktypes)) {
         // If no cycle filter or filter is verified
-        if ( this.selectedCycles.length === 0
+        if (this.selectedCycles.length === 0
           || this.elemIsFiltered(this.cycleToDict(this.events[event].cycle), this.selectedCycles)) {
           eventFiltered.push(this.events[event]);
         }
@@ -160,6 +175,102 @@ export class AdminCellComponent implements OnInit {
       }
     } else {
       this.filteredEvents = eventFiltered;
+    }
+  }
+
+
+  createForm() {
+
+    this.getCycles();
+    this.getTaskTypes();
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.eventForm = this.formBuilder.group(
+        {
+          nb_volunteers_needed: [0, [Validators.required, Validators.min(0)]],
+          cell_id: [params['cellId'], Validators.required],
+          cycle_id: [null, Validators.required],
+          task_type_id: [null, Validators.required],
+          start_date: [null, Validators.required],
+          end_date: [null, Validators.required]
+        },
+        {validator: this.dateValidator()
+        }
+      );
+    });
+  }
+
+  getCycles(): void {
+    this.cycleService.getCycles()
+      .subscribe(cycles => this.cycles = cycles.results);
+  }
+
+  getTaskTypes(): void {
+    this.tasktypeService.getTasktypes()
+      .subscribe(taskTypes => this.tasktypes = taskTypes.results);
+  }
+
+  dateValidator() {
+    return (group: FormGroup) => {
+
+      const date_start = group.controls['start_date'];
+      const date_end = group.controls['end_date'];
+
+      if (date_start.value && date_end.value && date_start.value >= date_end.value) {
+        return date_end.setErrors({
+          dateEndBeforeDateStart: true
+        });
+      }
+
+      if (!date_start.value && date_end.value) {
+        return date_start.setErrors({
+          dateStartMissing: true
+        });
+      }
+
+      if (date_start.value && !date_end.value) {
+        return date_end.setErrors({
+          dateEndMissing: true
+        });
+      }
+    };
+  }
+
+  createEvent() {
+    if (this.eventForm.valid) {
+
+      this.eventService.createEvent(this.eventForm.value).subscribe(
+        data => {
+          this.myModals.get('create event').toggle();
+          this.eventForm.reset();
+          this.notificationService.success('Création réussie',
+            `L'event "${data.name}" a été créé`);
+
+          this.get_events();
+        },
+        err => {
+
+          console.log(err);
+
+          let errorMessage = '';
+          if (err.error.nb_volunteers_needed) {
+            errorMessage += `nb_volunteers_needed erreur: ${err.error.nb_volunteers_needed[0]},`;
+          }
+          if (err.error.cycle_id) {
+            errorMessage += `cycle_id erreur: ${err.error.cycle_id[0]},`;
+          }
+          if (err.error.task_type_id) {
+            errorMessage += `task_type_id erreur: ${err.error.task_type_id[0]},`;
+          }
+          if (err.error.start_date) {
+            errorMessage += `Start date erreur: ${err.error.start_date[0]},`;
+          }
+          if (err.error.end_date) {
+            errorMessage += `End date erreur: ${err.error.end_date[0]},`;
+          }
+
+          this.myModals.get('create event').setErrorMessage(errorMessage);
+        }
+      );
     }
   }
 }
